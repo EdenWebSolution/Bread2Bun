baebcd302873d8a3c550e68b7765c50732c37d02
Thanzeel <tnlthanzeel@gmail.com>
2019-07-07 17:04:05 +0530

    implemented swagger

--- API/Bread2bun.API/Bread2Bun.API/Startup.cs
+++ API/Bread2bun.API/Bread2Bun.API/Startup.cs
@@ -9,6 +9,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Swashbuckle.AspNetCore.Swagger;
 
 namespace Bread2Bun.API
 {
@@ -25,6 +26,17 @@ namespace Bread2Bun.API
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info
+                {
+                    Version = "v1",
+                    Title = "Bread2Bun",
+                    Description = "Food ordering application",
+                    TermsOfService = "None"
+                });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -36,6 +48,12 @@ namespace Bread2Bun.API
             }
 
             app.UseMvc();
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Bread2Bun API");
+            });
         }
     }
 }
