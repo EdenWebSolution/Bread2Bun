d59be1258587b09317320f1477807bc35fc7d00a
Akmal Nawfer <akmal.nawfer@agileinnovationsgroup.com>
2019-08-16 02:37:14 +0530

    shared module added

--- web/Bread2Bun.Web/Bread2BunUI/src/app/modules/explore/explore.module.ts
+++ web/Bread2Bun.Web/Bread2BunUI/src/app/modules/explore/explore.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { ExploreRoutingModule } from './explore-routing.module';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
-    ExploreRoutingModule
+    ExploreRoutingModule,
+    SharedModule
   ]
 })
 export class ExploreModule { }
