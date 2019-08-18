420d33bd1b64e26187a453b5cccea4fd8461e23e
Akmal Nawfer <akmal.nawfer@agileinnovationsgroup.com>
2019-08-18 20:25:21 +0530

    cutom toastr styles added

--- web/Bread2Bun.Web/Bread2BunUI/src/app/modules/timeline/my-timeline/my-timeline.component.ts
+++ web/Bread2Bun.Web/Bread2BunUI/src/app/modules/timeline/my-timeline/my-timeline.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-my-timeline',
@@ -7,9 +8,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MyTimelineComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private t: ToastrService
+  ) { }
 
   ngOnInit() {
+    this.t.success('everything is broken', 'Major Error', {
+      progressBar: true,
+      toastClass: 'ngx-toastr my-success'
+    });
+
+    this.t.show('everything is broken', 'Major Error', {
+      progressBar: true,
+    });
+    this.t.error('everything is broken', 'Major Error', {
+      progressBar: true,
+    });
+    this.t.info('everything is broken', 'Major Error', {
+      progressBar: true,
+    });
+    this.t.warning('everything is broken', 'Major Error', {
+      progressBar: true,
+    });
   }
 
 }
