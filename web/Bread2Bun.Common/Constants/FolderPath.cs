aba67f787682dd9bda72599acd78989861a58329
Thanzeel <tnlthanzeel@gmail.com>
2019-09-24 22:18:42 +0530

    email changes

--- web/Bread2Bun.Common/Constants/FolderPath.cs
+++ web/Bread2Bun.Common/Constants/FolderPath.cs
@@ -7,11 +7,11 @@
         #endregion
 
         #region production
-        //public const string ImagePath = @"https://www.bread2bun.com/";
+        public const string ImagePath = @"https://www.bread2bun.com/";
         #endregion
 
         #region test
-        public const string ImagePath = @"http://test.bread2bun.com/";
+        //public const string ImagePath = @"http://test.bread2bun.com/";
         #endregion
 
         public const string ProfileImages = @"profileImages/";
